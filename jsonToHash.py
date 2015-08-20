0f8c1e8406b9f14d0f9359983529374ad96b1bc5
davidmrdavid <djusto@ucsd.edu>
2015-08-19 17:06:15 -0700

    Update jsonToHash.py

--- jsonToHash.py
+++ jsonToHash.py
@@ -2,7 +2,6 @@ import sys # Command line argument detection
 import gzip #For opening gzip compressed files 
 import pickle #For object serialization into files
 
-HASHMAP = {}
 
 #Constant strings
 DONE_STRING        = "Done writting to file: %s"
@@ -15,33 +14,65 @@ def parse(path):
 
 
 def main():
+	HASHMAP = {}
+	HASHMAPTEST = {}
+	print 'here'
+
 	#Parsing file to read
 	fileToParse = sys.argv[1]
 	outPutFile  = 'PRE-PROCESSED_' + fileToParse[10:]
 
 	generator = parse(fileToParse)
+	flag = True
 
-	while True:
+	handle = open( outPutFile , 'wb')
+	threshold = 50000
+		
+	nextJSON = next(generator)
+	currentKey = nextJSON['asin']
+	numDumps = 0
+
+	while flag:
+		c = 0
 		try:
-			currentJSON = next(generator)
-			currentKey = currentJSON['asin']
+			
+			while not( c > threshold and currentKey != nextJSON['asin']): 
+				try:
+					c = c + 1
+					currentJSON = nextJSON
+					
+					currentKey = currentJSON['asin']
 
-			if( currentKey in HASHMAP ):
-				HASHMAP[currentKey].append(currentJSON)
-			else:
-				HASHMAP[currentKey] = []
-				HASHMAP[currentKey].append(currentJSON)
-			print( HASHMAP )
+					if( currentKey in HASHMAP ):
+						HASHMAP[currentKey].append(currentJSON)
+						#HASHMAPTEST[currentKey].append(currentJSON)
+					else:
+						HASHMAP[currentKey] = []
+						HASHMAP[currentKey].append(currentJSON)
+						#HASHMAPTEST[currentKey] = []
+						#HASHMAPTEST[currentKey].append(currentJSON)
+					#print( HASHMAP )
+
+					nextJSON 	= next(generator)
+				except StopIteration:
+					flag = False 
+					break
+
+			pickle.dump(HASHMAP, handle)
+			numDumps = numDumps + 1
+			print 'dumping %d' %numDumps
+			HASHMAP = {}
+
 		except StopIteration:
+			flag = False 
 			break
 
-	with open( outPutFile , 'wb') as handle:
-		pickle.dump(HASHMAP, handle)
+	handle.close()
 
-	'''with open('file.txt', 'rb') as handle:
-		b = pickle.loads(handle.read())'''
+	'''with open( outPutFile, 'rb') as handle:
+		b = pickle.loads(handle.read())
 
-	#print HASHMAP == b # True
+	print HASHMAPTEST == b # True'''
 
 #Execution Start...
 main()
